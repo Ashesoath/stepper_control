15412b134858a4f93c07c83f277d56f79e1199d7
systec-electronic <vladyslav.tkachenko@systec-electronic.com>
2016-08-17 16:11:41 +0200

    Update stepmotorsub.py

--- stepmotorsub.py
+++ stepmotorsub.py
@@ -1,6 +1,59 @@
 #!/usr/bin/env python
-# SYS TEC electronic
-# Tkachenko Vladyslav
+#****************************************************************************
+#  Copyright, 2016, SYSTEC electronic GmbH, D-08468 Heinsdorfergrund, Am Windrad 2
+#  www.systec-electronic.com
+#
+#  Project: How to manage a stepper motor via ROS?
+#
+#  Description:  The Subscriber Node for system with Step motor 28BYJ-48. Receive the data. Starting the motor in the specified direction.
+#
+#  -------------------------------------------------------------------------
+#
+#  Author: 		Vladyslav Tkachenko
+#  Revision: 	1.0  
+#  Date: 		2016/08/17 14:59:59
+#
+#  -------------------------------------------------------------------------
+#
+#  Revision History:
+#
+#  2016/08/01 -rs:   Initial Implementation
+#
+#  -------------------------------------------------------------------------
+#  
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without 
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice, 
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice, 
+#     this list of conditions and the following disclaimer in the documentation 
+#	 and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors 
+#     may be used to endorse or promote products derived from this software 
+#	 without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+#  POSSIBILITY OF SUCH DAMAGE.*/
+# ****************************************************************************/
+# I want to express my gratitude to author Matt from web-site http://www.raspberrypi-spy.co.uk/author/matt/.
+# Lines of code have been taken from this site, which helped in the implementation of the system.
+# Thank you for a wonderful code, I recommend to visit his page on the web-site http://www.raspberrypi-spy.co.uk/.
+# ****************************************************************************/
+
 
 # Import libraries
 import rospy
